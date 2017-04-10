25ec4ba5e0b641e3be55528f7908d3c8cb386e36
Michael Harrison <mharrison13@cnm.edu>
2017-04-10 13:49:52 -0600

    color change

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 		<p><i class="fa fa-arrow-circle-right"></i> I have a degree in <strong>Interactive Multimedia and Advanced Graphic Design.</strong>
 			<br><i class="fa fa-arrow-circle-right"></i>  I am passionate about combining function, awesome design, and user experiences that make people happy.<br><i class="fa fa-arrow-circle-right"></i>   I love
 			creating challenging, intuitive and beautiful products.<br><i class="fa fa-arrow-circle-right"></i>   My design process is very hands-on and visual.</p>
-			<p><i class="fa fa-arrow-circle-right"></i>I also enjoy <em>Photography, Travel, Hiking, Yoga and spending time with my boys!</em></p>
+			<p><i class="fa fa-arrow-circle-right"></i> I also enjoy <em>Photography, Travel, Hiking, Yoga and spending time with my boys!</em></p>
 		<p><img src="images/header-bg.jpg" height="480" alt="kelly"></p>
 	</body>
 
