ab0fdd2fb9fca99b20dd0203e2003b73d5013367
kellykristl <goddesswebdesign@gmail.com>
2017-04-10 11:33:44 -0600

    about me assignment

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<body>
-<p>I am Kelly.
-	A designer extraordinare. I have a degree in Interactive Multimedia and Advanced Graphic Design.
-	I am passionate about combining function, awesome design, and user experiences that make people happy. I love creating
-	challenging, intuitive and beautiful products. My design process is very hands-on and visual.
-	I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</p>
+	</head>
+	<body>
+		<p>I am Kelly.
+			A designer extraordinare.<p>I have a degree in Interactive Multimedia and Advanced Graphic Design.
+			<br>I am passionate about combining function, awesome design, and user experiences that make people happy.<br> I love
+			creating
+			challenging, intuitive and beautiful products.<br> My design process is very hands-on and visual.</p>
+			<p>I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</p>
 		<p><img src="images/header-bg.jpg" height="480" width="640" alt="kelly"></p>
 	</body>
-	</head>
+
 </html>
