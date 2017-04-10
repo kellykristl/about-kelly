bf72a0752aed038a3e2aac8779d046c2cd529e15
Michael Harrison <mharrison13@cnm.edu>
2017-04-10 14:04:35 -0600

    color change

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
 				text-align: center;
 			}
 			h1 {
-				font-family: Lobster, times, Monospace;
+				font-family: Lobster, times;
 			}
 		</style>
 	</head>
 	<body>
 		<p><h1>I am Kelly. A designer extraordinaire.</h1>
-		<p><h2></h2><i class="fa fa-code"></i> I have a degree in <strong>Interactive Multimedia and Advanced Graphic Design.</strong>
+		<p><i class="fa fa-code"></i> I have a degree in <strong>Interactive Multimedia and Advanced Graphic Design.</strong>
 			<br><i class="fa fa-smile-o"></i>  I am passionate about combining function, awesome design, and user experiences that make people happy.<br><i class="fa fa-arrow-circle-right"></i>   I love
 			creating challenging, intuitive and beautiful products.<br><i class="fa fa-eye"></i>   My design process is very hands-on and visual.</p>
-		<p><i class="fa fa-camera"></i> I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</h2></em></p>
+		<p><i class="fa fa-camera"></i> I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</em></p>
 		<p><img src="images/header-bg.jpg" height="480" alt="kelly"></p>
 	</body>
 
