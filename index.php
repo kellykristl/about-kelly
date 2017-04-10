82be5e0cdb6d26d76277b26fd7e308a258b7816b
Michael Harrison <mharrison13@cnm.edu>
2017-04-10 13:13:55 -0600

    commit test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<br>I am passionate about combining function, awesome design, and user experiences that make people happy.<br> I love
 			creating
 			challenging, intuitive and beautiful products.<br> My design process is very hands-on and visual.</p>
-			<p>I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!!</p>
+			<p>I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</p>
 		<p><img src="images/header-bg.jpg" height="480" width="640" alt="kelly"></p>
 	</body>
 
