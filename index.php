fa7c9831f5eb68995add7d78564d5c842cb80c07
Michael Harrison <mharrison13@cnm.edu>
2017-04-10 14:39:40 -0600

    color change

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	<body>
 		<p><h1>I am Kelly. A designer extraordinaire.</h1>
 		<p><i class="fa fa-code"></i> I have a degree in <strong>Interactive Multimedia and Advanced Graphic Design.</strong>
-			<br><i class="fa fa-smile-o"></i>  I am passionate about combining function, awesome design, and user experiences that make people happy.<p>like clean code!<p><br><img src="http://www.featurepics.com/StockImage/20100629/man-despair-stock-picture-1601192.jpg" height="480"><br><br><i class="fa fa-arrow-circle-right"></i>   I love
+			<br><i class="fa fa-smile-o"></i>  I am passionate about combining function, awesome design, and user experiences that make people happy.<p><i class="fa fa-info"></i>like clean code!<p><br><img src="http://www.featurepics.com/StockImage/20100629/man-despair-stock-picture-1601192.jpg" height="480"><br><br><i class="fa fa-arrow-circle-right"></i>   I love
 		creating challenging, intuitive and beautiful products.<br><i class="fa fa-eye"></i>   My design process is very hands-on and visual.</p>
 		<p><i class="fa fa-camera"></i> I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</em></p>
 		<p><img src="images/header-bg.jpg" height="480" alt="kelly"></p>
