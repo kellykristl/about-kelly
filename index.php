71701e931887ccdc7d0d201c8604a3256ec4b3c8
Michael Harrison <mharrison13@cnm.edu>
2017-04-10 13:19:10 -0600

    color change

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 		</style>
 	</head>
 	<body>
-		<center><p><h1>I am Kelly. A designer extraordinaire.</h1>
+		<p><h1>I am Kelly. A designer extraordinaire.</h1>
 		<p>I have a degree in Interactive Multimedia and Advanced Graphic Design.
 			<br>I am passionate about combining function, awesome design, and user experiences that make people happy.<br> I love
 			creating
 			challenging, intuitive and beautiful products.<br> My design process is very hands-on and visual.</p>
 			<p>I also enjoy Photography, Travel, Hiking, Yoga and spending time with my boys!</p>
-		<p><img src="images/header-bg.jpg" height="480" width="640" alt="kelly"></p></center>
+		<p><img src="images/header-bg.jpg" height="480" width="640" alt="kelly"></p>
 	</body>
 
 </html>
